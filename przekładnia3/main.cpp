3cf035630f2b7c1acc1c7a6186fcb2af53d3052b
Oskar Nakielski <s1293221@student.pg.edu.pl>
2024-06-13 18:56:14 +0200

    Changeable simulation step, better looking ui( fixed input boxes, added indicators and better layout)

--- przekładnia3/main.cpp
+++ przekładnia3/main.cpp
@@ -1,10 +1,3 @@
-// Dear ImGui: standalone example application for DirectX 9
-
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
 
 #include "imgui.h"
 #include "imgui_impl_dx9.h"
@@ -31,23 +24,23 @@ void ResetDevice();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 // deklaracje zasięgów
-using namespace std;
+
+
 // stałe w programie
 #define N 2 // rząd systemu
-#define h 0.001 // krok obliczeń
 #define PI 3.14159265 // liczba PI
 
 // zmienne globalne w programie
 
 double T = 10.0; // całkowity czas symulacji – przedział [0 , T]
-std::vector <double> us((1.0 * T / h) + 1); // sygnał wejściowy 
-std::vector <double> I((1.0 * T / h) + 1); // sygnał wyjściowy
-std::vector <double> W((1.0 * T / h) + 1);
+double h = 0.001; // krok obliczeń
 double M = 8; // amplituda sygnału wejściowego
-
 double w = 1; //okres wygnalu wejsciowego
+std::vector <double> us((1.0 * T / h) + 1); // sygnał wejściowy 
+std::vector <double> I((1.0 * T / h) + 1); // natężenie prądu
+std::vector <double> W((1.0 * T / h) + 1); // prędkość kątowa
 
-//Box z; // zmienna: pojedyncza wartość sygnału (u lub y)
+
 
 
 // Main code
@@ -135,7 +128,7 @@ int main(int, char**)
         ImGui::SetNextWindowSize(ImGui::GetMainViewport()->Size);
         if (ImGui::Begin("Przekładnia", nullptr, ImGuiWindowFlags_NoDecoration))
         {
-            
+            ImGui::PushItemWidth(100);
             ImGui::InputDouble("J1", &param.J1, 1);
             ImGui::InputDouble("J2", &param.J2, 1);
             ImGui::InputDouble("n1", &param.n1, 1);
@@ -144,36 +137,59 @@ int main(int, char**)
             ImGui::InputDouble("L1", &param.L1, 1);
             ImGui::InputDouble("ke", &param.ke, 1);
             ImGui::InputDouble("kt", &param.kt, 1);
+            ImGui::PopItemWidth();
+            ImGui::Text("Krok obliczen: %f", h);
+            
+            if (ImGui::Button("0.01"))
+            {
+                
+                h = 0.01;
+            }
+            ImGui::SameLine();
+            if (ImGui::Button("0.001"))
+			{
+				h = 0.001;
+			}
+            ImGui::SameLine();
+            if (ImGui::Button("0.0001"))
+            {
+                h = 0.0001;
+            }
+
+            ImGui::Text("Sygnal wejsciowy: %c", signal);
 
             if (ImGui::Button("Sinus"))
             {
                 signal = 's';
                 signal_panel = true;
             };
-
+            ImGui::SameLine();
             if (ImGui::Button("Prostokatna"))
             {
                 signal = 'p';
                 signal_panel = true;
 
             };
+            ImGui::SameLine();
             if (ImGui::Button("Trojkatna"))
             {
                 signal = 't';
                 signal_panel = true;
             };
-
+            ImGui::SameLine();
             if (ImGui::Button("Heavyside"))
             {
                 signal = 'h';
                 signal_panel = true;
             };
-
+            ImGui::SameLine();
             if (ImGui::Button("Dirac"))
             {
                 signal = 'd';
                 signal_panel = true;
             }
+            
+
 
             if (signal_panel)
             {
@@ -185,20 +201,27 @@ int main(int, char**)
                 if (signal == 's' || signal == 'p' || signal == 't')
                 {
                     ImGui::InputDouble("Amplituda", &M, 0.5);
-                    ImGui::InputDouble("T", &T, 0.5);
+                    ImGui::InputDouble("Czas symulacji", &T, 0.5);
                     ImGui::InputDouble("Okres", &w, 0.5);
                 }
                 else
                 {
                     ImGui::InputDouble("Wartosc", &M, 0.5);
-                    ImGui::InputDouble("T", &T, 0.5);
+                    ImGui::InputDouble("Czas symulacji", &T, 0.5);
                 }
 
             }
             us.resize((1.0 * T / h) + 1);
-
+            for(int i = 0; i < 10; i++)
+			{
+				ImGui::Spacing();
+			}
             int k = 0;
-            if (ImGui::Button("Plot"))
+            
+				
+			
+
+            if (ImGui::Button("Plot",ImVec2(200, 50)))
             {
                 showPlotWindow = true;
                 create_param(param);
@@ -208,8 +231,8 @@ int main(int, char**)
                 us.clear();
                 us.resize(total);
                 
-                double slope = (4.0 * M) / w; // Slope for the triangular wave
-                double current_value = M; // Start from -M
+                double slope = (4.0 * M) / w; // Slope dla sygnału trójkątnego
+                double current_value = M; // start od -M
                 bool increasing = true;
 
                 for (i = 0; i < total; i++) // obliczenie pobudzenia – sinus lub fala prostokątna
@@ -267,40 +290,51 @@ int main(int, char**)
             }
 
         }
-        ImGui::End();
+        if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape)) && showPlotWindow==false)
+        {
+            
+            done = true;
+        }
+
+       
 
         //Rysowanie wykresów
         if (showPlotWindow)
         {
-            ImGui::SetNextWindowSize(ImVec2(1000, 800), ImGuiCond_Always);
-            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
-            ImGui::Begin("Wykresy", &showPlotWindow); // Pass the address of the boolean variable to control the window's visibility
+            if (ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape)))
+            {
+                showPlotWindow = false;
+            }
 
-            // Plot for 'Us'
+            ImGui::SetNextWindowSize(ImVec2(1000, 1000), ImGuiCond_Always);
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Always);
+            ImGui::Begin("Wykresy", &showPlotWindow); // 
+
+            // Wykres dla 'Us'
             ImPlot::SetNextAxesToFit();
-            ImPlot::SetNextLineStyle(ImVec4(1.0f, 0.0f, 0.0f, 1.0f)); // Red color
+            ImPlot::SetNextLineStyle(ImVec4(1.0f, 0.0f, 0.0f, 1.0f)); // czerwnony kolor
             if (ImPlot::BeginPlot("Us"))
             {
                 ImPlot::PlotLine("Us", x_data.data(), us.data(), (1.0 * T / h));
                 ImPlot::EndPlot();
             }
 
-            ImGui::Spacing(); // Add spacing between plots
+            ImGui::Spacing(); 
 
-            // Plot for 'I'
+            // Wykres dla 'I'
             ImPlot::SetNextAxesToFit();
-            ImPlot::SetNextLineStyle(ImVec4(0.0f, 1.0f, 0.0f, 1.0f)); // Green color
+            ImPlot::SetNextLineStyle(ImVec4(0.0f, 1.0f, 0.0f, 1.0f)); // zielony kolor
             if (ImPlot::BeginPlot("I"))
             {
                 ImPlot::PlotLine("I", x_data.data(), I.data(), (1.0 * T / h));
                 ImPlot::EndPlot();
             }
 
-            ImGui::Spacing(); // Add spacing between plots
+            ImGui::Spacing(); 
 
-            // Plot for 'W'
+            // Wykres dla 'W'
             ImPlot::SetNextAxesToFit();
-            ImPlot::SetNextLineStyle(ImVec4(0.0f, 0.0f, 1.0f, 1.0f)); // Blue color
+            ImPlot::SetNextLineStyle(ImVec4(0.0f, 0.0f, 1.0f, 1.0f)); // Niebieski kolor
             if (ImPlot::BeginPlot("W"))
             {
                 ImPlot::PlotLine("W", x_data.data(), W.data(), (1.0 * T / h));
@@ -309,6 +343,8 @@ int main(int, char**)
 
             ImGui::End();
         }
+
+        ImGui::End();
 
         // Rendering
         ImGui::EndFrame();
